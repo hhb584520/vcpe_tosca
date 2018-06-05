92d813f7863368507a58e4f36bc41241839b5110
Huang Haibin <haibin.huang@intel.com>
2018-06-05 23:30:09 +0800

    add image name
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- generate_csar.sh
+++ generate_csar.sh
@@ -7,7 +7,11 @@ PREFIX="vcpe_"
 DIRS="infra vbrgemu vbng vgmux vgw"
 
 for dir in $DIRS; do
+    image_name=${dir}_base_ubuntu_1604 
     cd $ROOT_DIR/$dir
+    mv Artifacts/images/ubuntu16.04 Artifacts/images/${image_name}
+    sed -i "s/ubuntu16.04/${image_name}/g" ./MainServiceTemplate.yaml
+    sed -i "s/ubuntu16.04/${image_name}/g" ./Definitions/MainServiceTemplate.yaml
     zip -r $ROOT_DIR/$dir/$dir.csar Artifacts/ TOSCA-Metadata/ Definitions/ MainServiceTemplate.mf MainServiceTemplate.yaml
     rm -f $DEST/$dir.csar
     mv $ROOT_DIR/$dir/$dir.csar $DEST/
